50def399a405e6903b2ecd9749cd55da5eeb516b
gkrishnan724 <gkrishnan724@gmail.com>
2019-05-07 20:51:45 +0530

    Added hashMap

--- linkedList.h
+++ linkedList.h
@@ -3,6 +3,7 @@
 
 //Struct declarations
 typedef struct Node{
+    int key;
     int val;
     struct Node *next;
 
@@ -17,6 +18,11 @@ typedef struct linkedList{
 linkedList* create_linked_list();
 Node* append(linkedList*,int);
 void print_elements(linkedList*);
+void append_map(linkedList*,int,int);
+int get(linkedList*,int);
+int get_from_key(linkedList*,int);
+void delete_from_key(linkedList*,int);
+void delete(linkedList*,int);
 
 //Function definition
 linkedList* create_linked_list(){
@@ -50,6 +56,31 @@ Node* append(linkedList* list,int value){
     return newnode;
 }
 
+void append_map(linkedList* list,int key,int value){
+    if(list->length == 0){
+        Node* newnode = (Node*)malloc(sizeof(Node));
+        newnode->val = value;
+        newnode->key = key;
+        newnode->next = NULL;
+        list->length += 1;
+        list->head = newnode;
+    }
+
+    Node* head = list->head;
+    Node* curr = head;
+    while(curr->next != NULL){
+        curr  = curr->next;
+    }
+
+    Node* newnode = (Node*)malloc(sizeof(Node));
+    newnode->key = key;
+    newnode->val = value;
+    newnode->next = NULL;
+    curr->next = newnode;
+    list->length += 1;
+    
+}
+
 void print_elements(linkedList* list){
     Node* head = list->head;
     while(head != NULL){
@@ -71,6 +102,59 @@ int get(linkedList* list,int index){
     }
     else{
         return head->val;
+    }
+}
+
+void delete(linkedList * list,int index){
+    int cnt = 0;
+    Node *head = list->head;
+    Node *prev = head;
+    while(cnt < index && head != NULL){
+        prev = head;
+        head = head->next;
+        cnt += 1;
+    }
+    if(head == NULL){
+        return;
+    }
+    else{
+        prev->next = head->next;
+        head->next = NULL;
+        free(head);
+        list->length -= 1;
+    }
+}
+
+
+int get_from_key(linkedList* list,int key){
+    Node* head = list->head;
+    while(head != NULL && head->key != key){
+        head = head->next;
+    }
+    if(head == NULL){
+        return -234;
+    }
+    else{
+        return head->val;
+    }
+}
+
+void delete_from_key(linkedList* list,int key){
+    Node *head = list->head;
+    Node *prev = head;
+    while(head != NULL && head->key != key){
+        prev = head;
+        head = head->next;
+    }
+    if(head == NULL){
+        return;
+    }
+    else{
+        prev->next = head->next;
+        head->next = NULL;
+        free(head);
+        list->length -= 1;
+
     }
 }
 
---
+++ map.h
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "linkedList.h"
+
+//Structure Declaration
+#define MAX_SIZE 10000
+
+//Uses Chaining for collision
+
+typedef linkedList hashMap;
+
+//Function prototype
+hashMap* init_map();
+int hash_function(int);
+int get_item(hashMap*,int);
+void add_item(hashMap*,int,int);
+void remove_item(hashMap*,int);
+
+//Function definitions
+int hash_function(int key){
+    return key%MAX_SIZE;
+}
+
+hashMap* init_map(){
+    hashMap* map = (hashMap*)malloc(MAX_SIZE*sizeof(linkedList));
+    return map;
+}
+
+void add_item(hashMap* map,int key,int value){
+    int index = hash_function(key);
+    append_map(&map[index],key,value);
+}
+
+int get_item(hashMap* map,int key){
+    int index = hash_function(key);
+    int item = get_from_key(&map[index],key);
+    return item;
+}
+
+void remove_item(hashMap* map,int key){
+    int index = hash_function(key);
+    delete_from_key(&map[index],key);
+}
